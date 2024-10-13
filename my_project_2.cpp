0964de0a4bb199f33f9c516e0b6fed88483dfea2
Artem77377 <artembahrov00@gmail.com>
2024-10-13 18:20:32 +0300

    changed file

--- my_project_2.cpp
+++ my_project_2.cpp
@@ -9,10 +9,8 @@ int main()
     cout << hex << 2209 << endl;
     cout  << right << showpos << setprecision(2) << -121.0 << endl;
     cout << showpos<< dec << +1  << +24 << endl;
-   
     cout << "String1" << '\n'<< '\t' << "String2" << '\n' << setw(23) << "String3" << endl;
     cout << setw(15) <<"String1" << '\n' << "String2" << endl;
     cout << 3;
     cout << "*";
-    cout << '\t' << "String" << '\n' << 1;
 }
