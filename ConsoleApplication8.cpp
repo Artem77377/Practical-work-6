209292a3d427d1727e86a5d75ae9ace638727ba2
Artem Bakhrov <artembahrov00@gmail.com>
2024-10-27 09:59:33 +0200

    new file is added to my personal branch

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+#include <math.h>;
+int main()
+{
+	float y, z, dob;
+	cout << "Enter y:\n";
+	cin >> y;
+	dob = 1;
+	for (int(k) = 1; k <= 5; k++) {
+		dob *= log(k * y);
+	}
+	z = (pow(y, 5) + dob);
+	cout << "z = " << z;
+}
