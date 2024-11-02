ad3bf8e4903f2fcfae2f89251de1b77af28cccf9
Artem Bakhrov <artembahrov00@gmail.com>
2024-11-02 16:47:53 +0200

    old commit, one changed file

--- my_project_2.cpp
+++ my_project_2.cpp
@@ -6,9 +6,4 @@ int main()
     cout << showpos << setw(8) << right << 4 << endl;
     cout << setw(14) << showpos << internal << fixed << setprecision(2) << -67.09124 << endl;
     cout << showpos << setw(8) << right << 235 << endl;
-    cout << "String1" << '\n'<< '\t' << "String2" << '\n' << setw(23) << "String3" << endl;
-    cout << setw(15) <<"String1" << '\n' << "String2" << endl;
-    cout << 3;
-    cout << "*";
-    cout << '\t' << "String" << '\n' << 1;
 }
