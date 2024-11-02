277cbb337591fd9244bc92dfcba93447326ada3c
Artem Bakhrov <artembahrov00@gmail.com>
2024-11-02 16:10:20 +0200

    one file deleted and another one is changed

--- ConsoleApplication7.cpp
+++ ConsoleApplication7.cpp
@@ -4,14 +4,14 @@ using namespace std;
 #include "math.h"
 int main()
 {
-	double x, y, x1, x2, h;
+	float x, y, x1, x2, h;
 	cout << "Enter [x1; x2] and step h\n";
 	cin >> x1 >> x2 >> h;
 	cout << "****************WHILE****************" << endl;
 	x = x1;
 	while (x <= x2 + h)
 	{
-		y = (log(pow(x, 2)) + pow(x, 2) + 2);
+		y = (log(pow(x, 34)) + pow(x, 2) + 2);
 		cout << setw(10) << "x = " << x << setw(15) << "y = " << y << endl;
 		x += h;
 	}
@@ -19,7 +19,7 @@ int main()
 	x = x1;
 	do
 	{
-		y = (log(pow(x, 2)) + pow(x, 2) + 2);
+		y = (log(pow(x, 2)) + pow(x, 2) + 90);
 		cout << setw(10) << "x = " << x << setw(15) << "y = " << y << endl;
 		x += h;
 	} while (x <= x2 + h);
